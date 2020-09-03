b111253d64c27b8d88ce8e5e644cdcaa052715cd
MornsCanCode <marvellousn561@gmail.com>
2020-09-04 00:31:36 +0100

    added fonts

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
 const searchIcon = document.querySelector('button')
 const input = document.querySelector('input')
-const cancel = document.querySelector('#Cancel')
 const searchForm = document.querySelector('form')
+const openInput = document.querySelector('#buttonPlaceHolder')
+const resultPage = document.querySelector('#resultPage')
+const closePage = document.querySelector('#closePage')
+const showAll = document.querySelector('#showAll')
+let RecipeList = 
+{
+  recipeName: ['Rice','egg','beans','eggstew','Milk','fishrolette','eggovine'],
+  // ingredients: {
+    
+  // }
+}
+
+console.log(RecipeList)
 
 searchIcon.addEventListener('click', () => {
-  input.style.width = '15vw'
-  input.style.cursor = 'text'
-  cancel.style.display = 'inline-block'
-  searchIcon.style.display = 'none'
-})
-
-cancel.addEventListener('click', () => {
   input.style.width = '0vw'
   input.style.cursor = 'not-allowed'
-  cancel.style.display = 'none'
-  searchIcon.style.display = 'inline-block'
+  openInput.style.display = 'inline-block'
+  searchIcon.style.display = 'none'
 })
 
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault()
-  console.log(input.value)
+  // console.log(input.value)
+  // searchArray()
 })
+
+openInput.addEventListener('click', () => {
+  input.style.width = '15vw'
+  input.style.cursor = 'text'
+  openInput.style.display = 'none'
+  searchIcon.style.display = 'inline-block'
+})
+
+closePage.addEventListener('mouseup', () => {
+  resultPage.style.width = '0vw'
+  resultPage.style.height = '0vh'
+  closePage.style.display = 'none'
+  document.querySelector('ul').style.display = 'none'
+})
+
+showAll.addEventListener('mouseup', () => {
+  resultPage.style.width = '80vw'
+  resultPage.style.height = '80vh'
+  closePage.style.display = 'inline-block'
+  document.querySelector('ul').style.display = 'flex'
+  
+})
+
+// let searchArray = () => {
+//   console.log(RecipeList[recipeName[indexOf(input.value)]])
+// } 
